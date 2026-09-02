407f9e8a63f49c1f9179e221d4234da7c0e9d285
brunaldihno <111083391+brunaldihno@users.noreply.github.com>
2026-09-02 17:58:26 -0400

    Update Analog_UART.cpp

--- Exp1/ESP32/Analog_UART.cpp
+++ Exp1/ESP32/Analog_UART.cpp
@@ -5,18 +5,20 @@ const int TX_PIN = 25;
 const int ADC_PIN = 34;
 
 void setup() {
-  //Serial.begin(115200);
+  Serial.begin(115200);
   MySerial.begin(9600, SERIAL_8N1, -1, TX_PIN);
 }
 
 void loop() {
   static uint16_t mv = 0;
-
+ 
   mv = analogReadMilliVolts(ADC_PIN);
   
   // Enviar 2 bytes, LSB primero
   MySerial.write((uint8_t)(mv >> 8));    // Byte alto
   MySerial.write((uint8_t)(mv & 0xFF));  // Byte bajo
+  Serial.print(">mv:");
+  Serial.println(mv);
 
-  //Serial.println(mv);
+  delay(5);
 }
